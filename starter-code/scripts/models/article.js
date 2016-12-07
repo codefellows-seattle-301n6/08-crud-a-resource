869f7c59079d2c4bcc68a0fd6a57b85da3ba8c6e
Dana Kulp <dkulp23@gmail.com>
2016-12-06 19:57:16 -0800

    added spaces to remove error code.

--- starter-code/scripts/models/article.js
+++ starter-code/scripts/models/article.js
@@ -12,7 +12,7 @@
 
   Article.prototype.toHtml = function(scriptTemplateId) {
     var template = Handlebars.compile(scriptTemplateId.text());
-    this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
+    this.daysAgo = parseInt((new Date() - new Date(this.publishedOn)) / 60 / 60 / 24 / 1000);
     this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
     this.body = marked(this.body);
     return template(this);
